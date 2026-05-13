26d66ad94d454fd91f7a17b631ec2b80acc1e998
belamueller <mail@belamueller.ch>
2026-05-13 15:14:34 +0200

    notFlippedNumbersString

--- puzzle.ts
+++ puzzle.ts
@@ -1,11 +1,24 @@
 export class Puzzle {
   flippedNumbers: number[] = [];
-  notFlippedNumbers: number[] = [1,2,3,4,5,6,7,8,9]
+  notFlippedNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
   flippedNumbersString(): string {
     let string = ""
     for(let i = 1; i <= 9; i++) {
-      if (i in this.flippedNumbers) {
+      if (this.flippedNumbers.includes(i)) {
+        string += i.toString();
+      }
+      else {
+        string += " "
+      }
+    }
+    return string;
+  }
+
+  notFlippedNumbersString(): string {
+    let string = ""
+    for(let i = 1; i <= 9; i++) {
+      if (this.notFlippedNumbers.includes(i)) {
         string += i.toString();
       }
       else {
